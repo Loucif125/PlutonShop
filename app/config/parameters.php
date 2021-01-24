700af27a26e01e447811d0ee6e35f33056806e8b
Loucif125 <loucif.khabache@etu.uca.fr>
2021-01-24 19:20:11 +0100

    App classes notification

---
+++ app/config/parameters.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'plutonshop',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'j0mwqpmMv6xAnq9mExnOgYWFWhY9HYFubo4sYbSHOQWWhw1T4oCew7BvopKkIooo',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2021-01-21',
+    'locale' => 'fr-FR',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'ZwLQ4XW5vJudoM5Dcf6DxSfFVYC5etk7e5paOdUx51S5UZWG0d6tPcxf8yL3SSBf',
+    'cookie_iv' => 'bgDcc9kw0SkQ6ne3DWsimtJGYIQRY0iE',
+    'new_cookie_key' => 'def00000d84e00b66f9b8d16fd6074ff19a6c3a6a8a1dea25347fc126dbc69abdb5a8b238ce4916d8d762110b75ff129ceda64533c6b1ea05f16844971e4670e372cbf9c',
+  ),
+);
